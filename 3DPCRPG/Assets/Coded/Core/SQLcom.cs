513212233e2c735958bcd602f8f231ddc5701aa9
Toufu Sekka <wint444@naver.com>
2019-12-09 21:51:10 +0900

    2019.12.09

---
+++ 3DPCRPG/Assets/Coded/Core/SQLcom.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SQLcom{
+    static readonly string DATASERVER_NAME="";
+    static readonly string DATASERVER_DATA = "";
+    static readonly string DATASERVER_USER = "";
+    static readonly string DATASERVER_PASS = "";
+
+    MySqlConnection connenction = new MySqlConnection("Server="+DATASERVER_NAME
+        +";Database="+ DATASERVER_DATA
+        +";Uid="+ DATASERVER_USER
+        + "Pwd"+ DATASERVER_PASS);
+    MySqlCommand command = new MySqlCommand();
+
+    
+    void init() {
+        //
+    }
+
+    void final() {
+        //
+    }
+}
---
+++ AUnitSTRBattleSimulation/Assets/Code/Data/UnitStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats{
+    // 000
+    public string Nations, Gender, userName;
+
+    // 010
+    public int Max_Life, Life, LUP_Life, Regen_Life, RegenLUP_Life;
+
+    // 020
+    public string SpecialGage;
+    public int Max_Gages, Gages, LUP_Gages, Regen_Gages, RegenLUP_Gages;
+
+    // 030
+    public string Armor;
+    public int Defend, LUP_Defend, Protection;
+
+    // 040
+    public string Weapon, DamageType;
+    public int Damage, LUP_Damage;
+
+    // 050
+    public int AttackSpeed, WalkSpeed, RunSpeed;
+
+    // 060
+    public Specialist Specialist;
+    public Ability[] ability;
+
+    public UnitStats(string[] StatData) {
+
+    }
+}
