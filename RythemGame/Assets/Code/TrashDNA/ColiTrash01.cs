5b1e1a90ff5d2a76621a81d8296b953930055019
Toufu Sekka <wint444@naver.com>
2019-12-11 04:43:15 +0900

    2019.12.11

---
+++ RythemGame/Assets/Code/TrashDNA/ColiTrash01.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColiTrash01 : MonoBehaviour{
+
+    GameObject obj;
+    private void Start(){
+        obj = GameObject.Find("Line");
+    }
+    private void Update(){
+        obj.transform.Rotate(new Vector3(0,0,-1));
+    }
+    private void OnTriggerEnter2D(Collider2D col2d){
+        if (col2d.gameObject.CompareTag("NoteType1"))
+            Debug.Log("touched");
+    }
+
+    private void OnTriggerExit2D(Collider2D col2d){
+        if (col2d.gameObject.CompareTag("NoteType1"))
+            Debug.Log("get out");
+    }
+}
