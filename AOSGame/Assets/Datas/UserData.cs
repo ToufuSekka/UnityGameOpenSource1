ce6aab90ae8ddbb929264207c2bfd36b93710848
Toufu Sekka <wint444@naver.com>
2020-02-25 15:56:17 +0900

    2020.02.25

---
+++ AOSGame/Assets/Datas/UserData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData : MonoBehaviour{
+    public int MaxLife, Life;
+    public int MaxStem, Stemina;
+    public int MaxMana, Mana;
+    public float Heart, Hunger, Spirit;
+    public float LifeRegen, StemRegen, ManaRegen;
+
+    public int Damage, Armor;
+
+    public EventData Events;
+
+    public string UserName, Class;
+
+}
