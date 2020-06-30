01e4e786953cb9ac4f654507eab99cfad08390e3
冬風雪花 <wint444@naver.com>
2020-07-01 08:27:53 +0900

    2020.07.01.2

---
+++ RythemGame/Assets/Code/System/Network.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+
+public class Network : MonoBehaviour{
+
+    public void init() {
+
+    }
+}
