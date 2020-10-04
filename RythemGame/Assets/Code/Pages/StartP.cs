3f4dfe16680ef9db1c644ca95cc55d92cb049697
冬風雪花 <wint444@naver.com>
2020-10-04 23:16:49 +0900

    2020. 10. 04

---
+++ RythemGame/Assets/Code/Pages/StartP.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartP : MonoBehaviour{
+
+    void Start() {
+
+    }
+
+
+    //if clicked Page, check Adress
+    private void clicked() {
+
+    }
+
+}
--- RythemGame/Assets/Code/TrashDNA/NewBehaviourScript.cs
+++ RythemGame/Assets/Code/TrashDNA/NewBehaviourScript.cs
@@ -2,6 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using Facebook.Unity;
+
 public class NewBehaviourScript : MonoBehaviour{
     //LogOn Testings
+
+    //UnityCallback
+    private void Awake() {
+        if (!FB.IsInitialized) {
+            FB.Init(FBINIT, OnHideUnity);
+            Debug.Log("Success INIT");
+        } else FB.ActivateApp();
+    }
+
+    //FaceBookCallback
+    void FBINIT() {
+        if (FB.IsInitialized) FB.ActivateApp();
+        else Debug.Log("INIT Failed!");
+    }
+    void OnHideUnity(bool isGameShown) {
+        if (!isGameShown) Time.timeScale = 0;
+        else Time.timeScale = 1;
+    }
+
+    void AuthCallback(ILoginResult result) {
+        var Token = AccessToken.CurrentAccessToken;
+
+        if (result.Error == null) Debug.Log(Token.ToString());
+        else Debug.Log("Failed LogIn : " + result.Error.ToString());
+    }
+
+    //custom
+
+    public void Clicked() {
+        if(!FB.IsLoggedIn)
+            FB.LogInWithReadPermissions(new List<string> { "public_profile" }, AuthCallback);
+    }
 }
